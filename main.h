9b6996257896e2223cb5292b78990027b4a3e0a7
Hamsa1913 <Hamsa12021@gmail.com>
2025-11-19 16:09:10 +0000

    Update main.h: add prototype for print_int to support %d and %i

--- main.h
+++ main.h
@@ -11,6 +11,7 @@ int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(void);
+int print_int(va_list args);
 
 #endif /* MAIN_H */
 
