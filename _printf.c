f54d66c9514859ac934599525e897ccc76aad1d5
Your Name <you@example.com>
2025-11-23 12:04:22 +0000

    feat: Implement %b specifier for binary conversion
    •⁠  ⁠Add print_binary function that converts unsigned int to binary
    •⁠  ⁠Handle %b format specifier in _printf function
    •⁠  ⁠Use division by 2 algorithm to extract binary digits
    •⁠  ⁠Return correct character count for proper formatting

--- _printf.c
+++ _printf.c
@@ -1,54 +1,69 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * print_binary - converts unsigned int to binary
+ * @num: number to convert to binary
+ * Return: number of characters printed
+ */
+int print_binary(unsigned int num)
+{
+	char binary[32];
+	int i = 0, len = 0;
+
+	if (num == 0)
+		return (write(1, "0", 1));
+
+	while (num > 0)
+	{
+		binary[i++] = (num % 2) + '0';
+		num /= 2;
+	}
+
+	len = i;
+	while (i > 0)
+		write(1, &binary[--i], 1);
+
+	return (len);
+}
 
 /**
  * _printf - produces output according to a format
- * @format: format string
- *
+ * @format: format string containing conversion specifiers
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0;
 	va_list args;
-
-	if (format == NULL)
-		return (-1);
+	int i = 0, count = 0;
 
 	va_start(args, format);
 
-	while (format[i] != '\0')
+	if (!format)
+		return (-1);
+
+	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-
-			if (format[i] == '\0')
-			{
-				va_end(args);
+			if (!format[i])
 				return (-1);
-			}
 
-			if (format[i] == 'c')
-				count += print_char(args);
-			else if (format[i] == 's')
-				count += print_string(args);
+			if (format[i] == 'b')
+				count += print_binary(va_arg(args, unsigned int));
 			else if (format[i] == '%')
-				count += print_percent();
-			else if (format[i] == 'd' || format[i] == 'i')
-				count += print_int(args);
+				count += write(1, "%", 1);
 			else
 			{
-				write(1, "%", 1);
-				write(1, &format[i], 1);
-				count += 2;
+				count += write(1, "%", 1);
+				count += write(1, &format[i], 1);
 			}
+			i++;
 		}
 		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		i++;
+			count += write(1, &format[i++], 1);
 	}
 
 	va_end(args);
