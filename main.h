74488739471f0d1665ecac52a26f8077b881168b
Your Name <you@example.com>
2025-11-23 12:03:29 +0000

    feat: Add function prototype for binary conversion
    •⁠  ⁠Add print_binary function prototype for %b specifier support

--- main.h
+++ main.h
@@ -1,17 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <unistd.h>
-
-/* main printf function */
 int _printf(const char *format, ...);
+int print_binary(unsigned int num);
 
-/* helper functions */
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(void);
-int print_int(va_list args);
-
-#endif /* MAIN_H */
+#endif
 
